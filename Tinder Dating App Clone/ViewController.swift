be22e78f143be43b0a45e589e047c6e8d1a5772a
Tony-Kara <eniolaanthony@gmail.com>
2020-06-30 00:09:19 +0600

    Initial Commit

---
+++ Tinder Dating App Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinder Dating App Clone
+//
+//  Created by mac on 6/30/20.
+//  Copyright © 2020 EniTony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
