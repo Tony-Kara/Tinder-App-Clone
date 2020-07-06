78d32abcf47fe3d1fb566ccf78810d68f824eb20
Tony-Kara <eniolaanthony@gmail.com>
2020-07-07 02:23:16 +0600

    Finished setting UI view

--- Tinder Dating App Clone/Controllers/HomeViewController.swift
+++ Tinder Dating App Clone/Controllers/HomeViewController.swift
@@ -13,7 +13,7 @@ class HomeViewController: UIViewController {
     // Properties
     private let topStack = HomeNavigationStackView()// create an instance of the stackview to connect to it's member fields.
     private let buttomStack = ButtomControlStackview()
-    private let deckView: UIView = { // This is for my image, the middle view, this is the UI component is created
+    private let deckView: UIView = { // This is for my image, the middle view, this is how the UI component is created
         let view = UIView()
         view.backgroundColor = .systemPink
         view.layer.cornerRadius = 5
@@ -24,17 +24,29 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        configureCards()
     }
     
     //Helpers
+    
+    func configureCards() {
+        let cardView1 = CardView()
+        let cardView2 = CardView()
+        
+        deckView.addSubview(cardView1)
+        deckView.addSubview(cardView2)
+        
+        cardView1.fillSuperview()
+        cardView2.fillSuperview()
+    }
     
     func configureUI(){
         view.backgroundColor = .white
         
         let stack = UIStackView(arrangedSubviews: [topStack,deckView, buttomStack])
         stack.axis = .vertical
-        view.addSubview(stack) //This adds your main vertical stack as as a subview of the super view.
-        stack.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor)
+        view.addSubview(stack) //This adds your main vertical stackview as as a subview of the super view.
+        stack.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor) //We have set the stackview to fill the whole screen by setting height for both topstack and bottomStack without any for the deckView, this means that height will be automatically calculated for deckview
         stack.isLayoutMarginsRelativeArrangement = true
         stack.layoutMargins = .init(top: 0, left: 12, bottom: 0, right: 12)
         stack.bringSubviewToFront(deckView)
---
+++ Tinder Dating App Clone/View/CardView.swift
@@ -0,0 +1,119 @@
+//
+//  CardView.swift
+//  Tinder Dating App Clone
+//
+//  Created by mac on 7/1/20.
+//  Copyright © 2020 EniTony. All rights reserved.
+//
+
+import UIKit
+class CardView: UIView {
+// Properties
+private let gradientLayer = CAGradientLayer()
+    
+private let imageView: UIImageView = {
+    let iv = UIImageView()
+    iv.contentMode = .scaleAspectFill
+    iv.image = #imageLiteral(resourceName: "kelly2")
+    return iv
+}()
+
+private let infoLabel: UILabel = {
+    let label = UILabel()
+    label.numberOfLines = 2
+    
+    let attributedText = NSMutableAttributedString(string: "Jane Doe", attributes: [.font: UIFont.systemFont(ofSize: 32, weight: .heavy), .foregroundColor: UIColor.white])
+    
+    attributedText.append(NSAttributedString(string: "  20", attributes: [.font: UIFont.systemFont(ofSize: 24), .foregroundColor: UIColor.white]))
+    
+    label.attributedText = attributedText
+    
+    return label
+}()
+
+private lazy var infoButton: UIButton = {
+    let button = UIButton(type: .system)
+    button.setImage( #imageLiteral(resourceName: "photo_placeholder").withRenderingMode(.alwaysOriginal), for: .normal)
+    return button
+}()
+
+// Lifecycle
+
+
+    override init(frame: CGRect) {
+       super.init(frame: frame)
+        
+        configureGestureRecognizers()
+        
+        backgroundColor = .systemPurple
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        
+        addSubview(imageView)
+        imageView.fillSuperview()
+        
+        configureGradientLayers() // call this fucntion before you add infoLabel and infoButton, this allows both to be added ontop of the gradient layer
+        
+        addSubview(infoLabel)
+        infoLabel.anchor(left: leftAnchor, bottom: bottomAnchor, right: rightAnchor,paddingLeft: 16, paddingBottom: 16, paddingRight: 16)
+        
+        addSubview(infoButton)
+        infoButton.setDimensions(height: 40, width: 40)
+        infoButton.centerY(inView: infoLabel)
+        infoButton.anchor(right: rightAnchor, paddingRight: 16)
+    
+    }
+    
+    override func layoutSubviews() {
+        gradientLayer.frame = self.frame
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Actions
+    
+    @objc func handlePanGesture(sender: UIPanGestureRecognizer){
+        let translation = sender.translation(in: nil) // The translation gives the X AND Y positions
+        
+        switch sender.state{
+            
+      
+        case .began:
+            print("DEBUG: Pan did begin")
+        case .changed:
+            let degrees: CGFloat = translation.x / 20
+            let angle = degrees * .pi / 180
+            let rotationalTransform = CGAffineTransform(rotationAngle: angle)
+            self.transform = rotationalTransform.translatedBy(x: translation.x, y: translation.y)
+        case .ended:
+            print("DEBUG: Pan ended")
+        
+        default: break
+        }
+        
+        
+    }
+    
+    @objc func handleChangePhoto() {
+        
+    }
+    
+    //MARK: - Helpers
+    
+    func configureGradientLayers()  {
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradientLayer.locations = [0.5, 1.1]
+        layer.addSublayer(gradientLayer)
+        
+    }
+    
+    func configureGestureRecognizers() {
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture))
+        addGestureRecognizer(pan)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleChangePhoto))
+        addGestureRecognizer(tap)
+    }
+}
